eb7404b8950687c5d4a1a48e625844e72a8267ab
matias_gomez <mfgomez.810@gmail.com>
2021-10-27 01:55:41 -0300

    sectionicon responsive

--- src/components/SectionIcons.jsx
+++ src/components/SectionIcons.jsx
@@ -1,27 +1,27 @@
 const SectionIcons = () => {
   return (
-    <div className="container-fluid bg-primary">
+    <div className="container-fluid bg-primary py-5">
       <div className="container">
-        <div className="row  p-5">
-          <div className="col-3">
+        <div className="row">
+          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
             <div className="d-flex align-items-center justify-content-start">
               <img src={`assets/icon-envios.png`} alt="icon" />
               <span className="text-icon">Hacemos envíos a todo el país.</span>
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
             <div className="d-flex align-items-center justify-content-start">
               <img src={`assets/icon-atencion.png`} alt="icon" />
               <span className="text-icon">Atención personalizada.</span>
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
             <div className="d-flex align-items-center justify-content-start">
               <img src={`assets/icon-puerta-puerta.png`} alt="icon" />
               <span className="text-icon">Envíos puerta a puerta.</span>
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
             <div className="d-flex align-items-center justify-content-start">
               <img src={`assets/icon-seguimiento.png`} alt="icon" />
               <span className="text-icon">
