e7949b2b96c1ff25f64324ce3108711d03583b4b
matias_gomez <mfgomez.810@gmail.com>
2021-10-27 03:02:41 -0300

    section traffic responsive

--- src/components/MainTraffic.jsx
+++ src/components/MainTraffic.jsx
@@ -1,9 +1,9 @@
 const MainTraffic = ({button, background}) => {
   return (
-    <div className="container-fluid p-0 d-flex">
+    <div className="container-fluid section-traffic">
       <img src={`assets/trafic.jpg`} alt="trafic" className="main-trafic-img" />
       <div className={`${background} d-flex flex-column justify-content-center align-items-center text-left`}>
-        <span className="section-trafic-text-title text-white w-50">
+        <span className="section-trafic-text-title">
           Recorremos las rutas del paÍs llevando todos tus envÍos.
         </span>
         <span role="button" className={`btn-pill bg-${button}`}>
