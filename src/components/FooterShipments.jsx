298bdf2a9f327418fd16783402521a0d6579da59
matias_gomez <mfgomez.810@gmail.com>
2021-10-27 03:22:33 -0300

    footer shipments responsive

--- src/components/FooterShipments.jsx
+++ src/components/FooterShipments.jsx
@@ -11,13 +11,13 @@ const FooterShipments = ({image, button}) => {
       }}
     >
       <div className="footer-title">Seguí tu envio</div>
-      <div className="col-4 my-4">
+      <div className="col-8 col-md-4 my-4">
         <div className="footer-subtitle">
           Vas a poder hacer el seguimiento de tu envío en todo momento para tu
           mayor tranquilidad.
         </div>
       </div>
-      <div className="col-6">
+      <div className="col-10 col-md-5">
         <div className="d-flex align-items-center justify-content-center">
           <button type="button" className="step-button">
             <span>
