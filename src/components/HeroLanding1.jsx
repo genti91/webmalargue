c9ab0d068507aa5df11c4d0fd34789be2d5b5756
Gabriel Uccello <gabrielucc@gmail.com>
2022-03-15 18:08:10 -0300

    fix landings visuales anuncios

--- src/components/HeroLanding1.jsx
+++ src/components/HeroLanding1.jsx
@@ -2,15 +2,13 @@ import Form from "./Form";
 import FormEmpresas from "./FormEmpresas";
 const HeroLanding1 = ({ image, title, page }) => {
   return (
-    <div className={`${page?'wrapper-hero':'wrapper-hero-2' } bg-primary`}>
+    <div className={`${page ? "wrapper-hero" : "wrapper-hero-2"} bg-primary`}>
       <div
         className="hero-landing-1"
         style={{
           backgroundImage: `url(${image})`,
         }}
       >
-        
-        
         <span className="hero-title-big">{title[1]}</span>
       </div>
       {page ? <Form /> : <FormEmpresas />}
