0dcf6feeab4c29ec2a8f00bb20a48c3d7a8c3104
clod2008 <claudio@apsis.com.ar>
2022-03-31 13:11:46 -0300

    nueva NavBar

---
+++ src/components/NavBar/NavBar copy.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Logo } from "../../assets/Logo";
+import "./NavBar.scss";
+
+export const NavBar = () => {
+  const [navBar, setNavBar] = useState(false);
+
+  const links = [
+    {
+      to: "/",
+      name: "Home",
+    },
+    {
+      to: "/servicios",
+      name: "Servicios",
+    },
+    {
+      to: "/nosotros",
+      name: "Nosotros",
+    },
+    {
+      to: "/tracking",
+      name: "Seguimiento",
+    },
+    {
+      to: "/cotiza",
+      name: "Cotiza",
+    },
+    {
+      to: "/contacto",
+      name: "Contacto",
+    },
+  ];
+
+  const changeBacground = () => {
+    //https://www.youtube.com/watch?v=JMsNslI8KoY
+    if (window.scrollY >= 110) {
+      setNavBar(true);
+    } else {
+      setNavBar(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBacground);
+
+  return (
+    <nav
+      id="NavBarHome"
+      className={
+        navBar
+          ? "navbar navbar-expand-lg navbar-light bg-light fixed-top activeNav"
+          : "navbar navbar-expand-lg navbar-light bg-light fixed-top"
+      }
+    >
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          {<Logo />}
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse
+                          navbar-collapse
+                          flex-end
+                          d-flex 
+                          justify-content-end
+                          "
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            {links.map(({ to, name }) => (
+              <li key={name}>
+                <NavLink to={to}>{name}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,37 +1,53 @@
 import React, { useState } from "react";
+import { Container, Navbar, NavbarBrand, NavDropdown, Nav, NavItem } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { Logo } from "../../assets/Logo";
 import "./NavBar.scss";
 
+
+
 export const NavBar = () => {
   const [navBar, setNavBar] = useState(false);
 
-  const links = [
-    {
+  const links ={
+    home:{
       to: "/",
       name: "Home",
     },
-    {
-      to: "/servicios",
-      name: "Servicios",
+    servicios:{
+      name:"Servicios",
+      individuos:{
+        to: "/individuosserv",
+        name: "Individuos",
+      },
+      empresas:{
+        to: "/empresasserev",
+        name: "Empresas",
+      },
+      eCommerce:{
+        to: "/ecommerceserv",
+        name: "E-Commerce",
+      },
     },
-    {
+    nosotros:{
       to: "/nosotros",
       name: "Nosotros",
     },
-    {
+    tracking:{
       to: "/tracking",
       name: "Seguimiento",
     },
-    {
+    cotiza:{
       to: "/cotiza",
       name: "Cotiza",
     },
-    {
+    contacto:{
       to: "/contacto",
       name: "Contacto",
     },
-  ];
+    
+  }
+
 
   const changeBacground = () => {
     //https://www.youtube.com/watch?v=JMsNslI8KoY
@@ -45,7 +61,9 @@ export const NavBar = () => {
   window.addEventListener("scroll", changeBacground);
 
   return (
-    <nav
+    
+    <Navbar
+      variant="dark"
       id="NavBarHome"
       className={
         navBar
@@ -53,39 +71,91 @@ export const NavBar = () => {
           : "navbar navbar-expand-lg navbar-light bg-light fixed-top"
       }
     >
-      <div className="container">
-        <a className="navbar-brand" href="#">
-          {<Logo />}
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <Container>
         <div
-          className="collapse
-                          navbar-collapse
-                          flex-end
-                          d-flex 
-                          justify-content-end
-                          "
-          id="navbarNav"
-        >
-          <ul className="navbar-nav">
-            {links.map(({ to, name }) => (
-              <li key={name}>
-                <NavLink to={to}>{name}</NavLink>
-              </li>
-            ))}
-          </ul>
+        className=" ">
+          <a className="navbar-brand" href="/">
+            {<Logo />}
+          </a>
         </div>
-      </div>
-    </nav>
+        <Navbar.Toggle 
+         
+          aria-controls="navbar-dark-example" 
+        />
+          <Navbar.Collapse id="navbar-dark-example"
+            className="d-flex justify-content-end "
+          >
+            <Nav
+              className="d-flex align-items-center "
+            >
+              <NavItem>
+                <NavLink
+                    to={links.home.to}
+                >
+                    {links.home.name}
+                </NavLink>
+              </NavItem>
+              <NavDropdown
+                    id="nav-dropdown-dark-example"
+                    title={links.servicios.name}
+                    menuVariant="dark"
+                  >
+                    <NavDropdown.Item>
+                      <NavLink
+                        to={links.servicios.individuos.to}
+                      >
+                        {links.servicios.individuos.name}
+                      </NavLink>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                      <NavLink
+                        to={links.servicios.empresas.to}
+                      >
+                        {links.servicios.empresas.name}
+                      </NavLink>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                      <NavLink
+                        to={links.servicios.eCommerce.to}
+                      >
+                        {links.servicios.eCommerce.name}
+                      </NavLink>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                  </NavDropdown>
+              <NavItem>
+                <NavLink
+                    to={links.nosotros.to}
+                >
+                    {links.nosotros.name}
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                    to={links.tracking.to}
+                >
+                    {links.tracking.name}
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                    to={links.cotiza.to}
+                >
+                    {links.cotiza.name}
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                    to={links.contacto.to}
+                >
+                    {links.contacto.name}
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
