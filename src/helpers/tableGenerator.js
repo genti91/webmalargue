fe281116912995f080f85d7100361b8fcd2fb4cb
Marco Baiad <marcobaiad@gmail.com>
2022-06-28 15:29:01 -0300

    corrección en calculo somatoria metros total

--- src/helpers/tableGenerator.js
+++ src/helpers/tableGenerator.js
@@ -8,8 +8,9 @@ const sumatoriaHandler = (row) =>
 export const tableTemplateGenerator = ({ columns, dataSource }) => {
   let formedColumns = ''
   let formedRows = ''
-  let sumatoriaMetros = ''
-  let sumatoriaBultos = ''
+  let sumatoriaMetros = 0
+  let sumatoriaMetrosTotal = 0
+  let sumatoriaBultos = 0
 
   Object.values({ ...columns, sumatoria: 'Sumatoria m3' }).forEach((label) => {
     if (label !== 'Seleccionar')
@@ -24,6 +25,7 @@ export const tableTemplateGenerator = ({ columns, dataSource }) => {
             sumatoriaMetros = (
               (sumatoriaHandler(row) || 0) * Number(row?.cantBultos)
             ).toFixed(4)
+            sumatoriaMetrosTotal += Number(sumatoriaMetros)
             sumatoriaBultos += parseInt(row?.cantBultos) * parseInt(row?.peso)
             formedRows += `<td style="border: 0.5px solid #111; box-sizing: border-box;">${
               sumatoriaMetros || 0
@@ -35,8 +37,8 @@ export const tableTemplateGenerator = ({ columns, dataSource }) => {
     )
     formedRows += `</tr>`
   })
-
+  console.log(sumatoriaMetrosTotal);
   return `<table style="margin-top: 20px; border-spacing: 0px !important;"><thead><tr>${formedColumns}</tr></thead><tbody>${formedRows}</tbody></table><div><p style="margin-top: 10px; margin-bottom: 0px;">Peso Total: <span style="font-weight: bold;">${sumatoriaBultos}</span></p><p>Metros Aforados: <span style="font-weight: bold;">${(
-    (sumatoriaMetros || 0) * 350
+    (sumatoriaMetrosTotal || 0) * 350
   ).toFixed(4)}</span></><br></div>`
 }
