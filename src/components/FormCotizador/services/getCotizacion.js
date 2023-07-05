e35bda3a7a31457281a60da944d3ed66bb76d61f
genti91 <franco.g7@hotmail.com>
2023-07-05 20:27:56 -0300

    copy form

--- src/components/FormCotizador/services/getCotizacion.js
+++ src/components/FormCotizador/services/getCotizacion.js
@@ -1,13 +1,13 @@
 const { REACT_APP_API_KEY } = process.env
 
-export const getCotizacion = async (parametros) => {
+export const getCotizacion = async (props) => {
   const response = await window
     .fetch(
       `https://www.softwarecristal.com/web/apitest/public/?token=${REACT_APP_API_KEY}&o=cotizacion`,
       {
         method: 'POST',
         credentials: 'same-origin',
-        body: JSON.stringify(parametros),
+        body: JSON.stringify(props),
       }
     )
     .catch((error) => {
