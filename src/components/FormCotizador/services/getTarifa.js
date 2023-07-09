cf1ab09b53cdd54ee118e167c14244c1253a5acb
genti91 <franco.g7@hotmail.com>
2023-07-09 11:56:15 -0300

    fix get tarifa

--- src/components/FormCotizador/services/getTarifa.js
+++ src/components/FormCotizador/services/getTarifa.js
@@ -3,15 +3,10 @@ const { REACT_APP_API_KEY } = process.env
 export const getTarifa = async () => {
   const response = await window
     .fetch(
-      `https://www.softwarecristal.com/web/apitest/public/?token=${REACT_APP_API_KEY}&o=tarifa`,
+      `https://www.softwarecristal.com/web/apitest/public/?token=${REACT_APP_API_KEY}&o=verTarifa`,
       {
         method: 'GET',
         credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json',
-            'Authorization': 'd4fda7da-acfb-40b0-89ec-9e2b2898c2f2'
-        }
       }
     )
     .catch((error) => {
