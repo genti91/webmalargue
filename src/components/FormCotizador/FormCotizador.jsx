475286746b96d5e74f7a53bf801d5611f6c7c1d2
genti91 <franco.g7@hotmail.com>
2023-07-20 00:44:37 -0300

    fix: submit button position

--- src/components/FormCotizador/FormCotizador.jsx
+++ src/components/FormCotizador/FormCotizador.jsx
@@ -195,7 +195,7 @@ const FormCotizacion = (props) => {
                     </h2>
                   </Col>
                   {formCotiza.map((item, index) => (
-                    <Col {...item?.colSize} key={index}>
+                    <Col {...item?.colSize} style={item.inputProps.type === 'submit' ? { display:'flex', justifyContent:'end' } : null} key={index}>
                       {item?.label && <label>{item.label}</label>}
                       {item.inputProps.type.match(/text|select|email|tel/) && (
                         <>
