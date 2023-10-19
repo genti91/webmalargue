b2f20758b4ed8e22cef73ba6a88675a169dac238
genti91 <franco.g7@hotmail.com>
2023-10-19 14:55:41 -0300

    prod api key

--- src/components/FormCotizador/services/getTarifa.js
+++ src/components/FormCotizador/services/getTarifa.js
@@ -1,9 +1,9 @@
-const { REACT_APP_API_KEY } = process.env
+const { REACT_APP_API_KEY_COTIZA } = process.env
 
 export const getTarifa = async () => {
   const response = await window
     .fetch(
-      `https://www.softwarecristal.com/web/api/public/?token=${REACT_APP_API_KEY}&o=verTarifa`,
+      `https://www.softwarecristal.com/web/api/public/?token=${REACT_APP_API_KEY_COTIZA}&o=verTarifa`,
       {
         method: 'GET',
         credentials: 'same-origin',
