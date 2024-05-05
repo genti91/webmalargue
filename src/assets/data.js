13f32cb295b4e7511819ee1f1a6f7f4b673ee16f
sergioAQU <118860312+sergioAQU@users.noreply.github.com>
2024-05-05 20:04:03 -0300

    Se fusionan los cambios de tracking.js e index.html al cotizar actualizado

--- src/assets/data.js
+++ src/assets/data.js
@@ -7,7 +7,7 @@ const siteData = {
         display: '+54 9 11 6362-2778',
         num: '5491163622778',
     },
-    adress: '-AV. Garmendia 4805 CABA (1427)-',
+    adress: '',
     social: {
         facebook: 'https://www.facebook.com/Expresomalargue',
         faceDisplay: '/Expresomalargue',
