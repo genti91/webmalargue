93c5d34eb0d9d6f94cb0e77c173302de183b0cb6
nicotanoira <nicotanoira@gmail.com>
2025-06-06 12:48:44 -0300

    feat: modularized components for reusability

---
+++ src/components/CotizacionYRetiro/SavedBultosTable.jsx
@@ -0,0 +1,49 @@
+export const SavedBultosTable = ({ bultos }) => {
+    return (
+        <div className='tw-w-full tw-overflow-x-auto'>
+          <table className='tw-w-full tw-border tw-bg-white tw-whitespace-nowrap lg:tw-whitespace-normal'>
+            <thead>
+              <tr className='tw-border-b tw-border-[#222]'>
+                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                  Cantidad de bultos
+                </th>
+                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                  Peso unitario (kg)
+                </th>
+                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                  Ancho unitario (cm)
+                </th>
+                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                  Alto unitario (cm)
+                </th>
+                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                  Largo unitario (cm)
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {bultos.map((bulto, idx) => (
+                <tr key={idx} className={idx % 2 === 0 ? 'tw-bg-[#F5F5F5]' : ''}>
+                  <td className='tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                    {bulto?.quantity}{' '}
+                    {bulto?.quantity === '1' ? 'unidad' : 'unidades'}
+                  </td>
+                  <td className='tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                    {bulto?.weight}kg
+                  </td>
+                  <td className='tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                    {bulto?.width}cm
+                  </td>
+                  <td className='tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                    {bulto?.height}cm
+                  </td>
+                  <td className='tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                    {bulto?.length}cm
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+    )
+} 
---
+++ src/components/CotizacionYRetiro/calculatePriceDetail.js
@@ -0,0 +1,36 @@
+export const calculatePriceDetail = ({ totalAPIPrice }) => {
+    // Let's solve for x (base value) in the equation:
+    // x + (x * 0.01) + ((x + x*0.01) * 0.21) = total
+    // x + 0.01x + 0.21x + 0.0021x = total
+    // 1.2221x = total
+    // x = total / 1.2221
+    const noTaxPrice = (totalAPIPrice / 1.2221).toLocaleString('es-AR', {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2,
+      })
+      const seguroValue = (
+        Number(totalAPIPrice / 1.2221) * 0.01
+      ).toLocaleString('es-AR', {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2,
+      })
+      const ivaValue = (
+        (Number(totalAPIPrice / 1.2221) +
+          Number(totalAPIPrice / 1.2221) * 0.01) *
+        0.21
+      ).toLocaleString('es-AR', {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2,
+      })
+      const finalValue = Number(totalAPIPrice).toLocaleString('es-AR', {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2,
+      })
+      
+    return {
+        noTaxPrice,
+        seguroValue,
+        ivaValue,
+        finalValue
+    }
+}
