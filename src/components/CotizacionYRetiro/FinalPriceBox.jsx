d3176d939db9e20565096b1eec92dda47c0cf235
genti91 <franco.g7@hotmail.com>
2025-07-30 13:19:48 -0300

    css fixes

--- src/components/CotizacionYRetiro/FinalPriceBox.jsx
+++ src/components/CotizacionYRetiro/FinalPriceBox.jsx
@@ -2,7 +2,7 @@ export const FinalPriceBox = ({ price }) => {
 return (
     <div className='tw-flex tw-flex-col tw-justify-center tw-items-center tw-w-full'>
         <div className='tw-flex tw-flex-col tw-w-full tw-max-w-[350px] tw-justify-center tw-items-center text-center tw-rounded-lg tw-border tw-border-[#707070] '>
-          <span className='tw-text-[#2F3394] tw-text-2xl tw-font-semibold tw-py-2'>
+          <span className='tw-text-[#2F3394] tw-text-2xl tw-font-bold tw-py-2'>
             PRECIO FINAL
           </span>
           <span className='tw-w-full tw-text-white tw-text-2xl tw-font-semibold tw-bg-[#2F3394] tw-rounded-b-lg tw-py-2'>
--- src/components/CotizacionYRetiro/TarifaDetail.jsx
+++ src/components/CotizacionYRetiro/TarifaDetail.jsx
@@ -1,6 +1,6 @@
 export const TarifaDetail = ({ noTaxPrice, seguroValue, ivaValue }) => {
     return (
-        <div className='tw-flex tw-flex-col gap-4 tw-mb-9'>
+        <div className='tw-flex tw-flex-col tw-gap-4 tw-mb-9'>
         <span className='tw-text-[#2F3394] tw-text-2xl tw-font-semibold'>
           Detalle de la tarifa
         </span>
