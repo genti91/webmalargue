9c6b26e067c65c68e1d6611fc23a4d01b284ae5c
nicotanoira <nicotanoira@gmail.com>
2025-07-31 01:56:41 -0300

    fix: bultos table result styling to figma

--- src/components/CotizacionYRetiro/SavedBultosTable.jsx
+++ src/components/CotizacionYRetiro/SavedBultosTable.jsx
@@ -1,22 +1,22 @@
 export const SavedBultosTable = ({ bultos }) => {
     return (
-        <div className='tw-w-full tw-overflow-x-auto'>
-          <table className='tw-w-full tw-border tw-bg-white tw-whitespace-nowrap lg:tw-whitespace-normal'>
+        <div className='tw-w-full tw-overflow-x-auto tw-mt-6'>
+          <table className='tw-w-full tw-border tw-border-[#DEE2E6] tw-bg-white tw-whitespace-nowrap lg:tw-whitespace-normal tw-border-collapse'>
             <thead>
-              <tr className='tw-border-b tw-border-[#222]'>
-                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+              <tr className='tw-border-b-2 tw-border-black'>
+                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border-[#DEE2E6] tw-border-r tw-whitespace-nowrap lg:tw-whitespace-normal'>
                   Cantidad de bultos
                 </th>
-                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border-[#DEE2E6] tw-border-r tw-whitespace-nowrap lg:tw-whitespace-normal'>
                   Peso unitario (kg)
                 </th>
-                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border-[#DEE2E6] tw-border-r tw-whitespace-nowrap lg:tw-whitespace-normal'>
                   Ancho unitario (cm)
                 </th>
-                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border-[#DEE2E6] tw-border-r tw-whitespace-nowrap lg:tw-whitespace-normal'>
                   Alto unitario (cm)
                 </th>
-                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                <th className='tw-font-bold tw-text-left tw-py-2 tw-px-3 tw-border-[#DEE2E6] tw-border-r tw-whitespace-nowrap lg:tw-whitespace-normal'>
                   Largo unitario (cm)
                 </th>
               </tr>
@@ -24,20 +24,20 @@ export const SavedBultosTable = ({ bultos }) => {
             <tbody>
               {bultos.map((bulto, idx) => (
                 <tr key={idx} className={idx % 2 === 0 ? 'tw-bg-[#F5F5F5]' : ''}>
-                  <td className='tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                  <td className='tw-py-2 tw-px-3 tw-border-t tw-border-r tw-border-[#DEE2E6] tw-whitespace-nowrap lg:tw-whitespace-normal'>
                     {bulto?.quantity}{' '}
                     {bulto?.quantity === '1' ? 'unidad' : 'unidades'}
                   </td>
-                  <td className='tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                  <td className='tw-py-2 tw-px-3 tw-border-t tw-border-r tw-border-[#DEE2E6] tw-whitespace-nowrap lg:tw-whitespace-normal'>
                     {bulto?.weight}kg
                   </td>
-                  <td className='tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                  <td className='tw-py-2 tw-px-3 tw-border-t tw-border-r tw-border-[#DEE2E6] tw-whitespace-nowrap lg:tw-whitespace-normal'>
                     {bulto?.width}cm
                   </td>
-                  <td className='tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                  <td className='tw-py-2 tw-px-3 tw-border-t tw-border-r tw-border-[#DEE2E6] tw-whitespace-nowrap lg:tw-whitespace-normal'>
                     {bulto?.height}cm
                   </td>
-                  <td className='tw-py-2 tw-px-3 tw-border tw-whitespace-nowrap lg:tw-whitespace-normal'>
+                  <td className='tw-py-2 tw-px-3 tw-border-t tw-border-r tw-border-[#DEE2E6] tw-whitespace-nowrap lg:tw-whitespace-normal'>
                     {bulto?.length}cm
                   </td>
                 </tr>
